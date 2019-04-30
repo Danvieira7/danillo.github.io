9f4d8b2634041e26d84e671132717ceda0489b7f
DanVieira7 <gisellademorais@Gisellas-MacBook-Pro.local>
2019-04-29 17:10:20 -0700

    content and responsiveness updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
     $('nav a').on("click" , function(e){
         e.preventDefault();
-        var top = $($(this).attr('href')).offset().top - 105;
+        var top = $($(this).attr('href')).offset().top - 110;
         $("html, body").animate({ scrollTop: top }, 800);
     });
 })
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
