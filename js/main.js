60aefe5c0921bf37f84c6e193b613eaa7f80a752
DanVieira7 <gisellademorais@Gisellas-MacBook-Pro.local>
2019-05-01 20:06:30 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,51 +1,17 @@
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
-
-// $('.scroll').on("click", function(e){
-//     e.preventDefault();
-//     var dest =$(this).attr("href");
-//     $("html, body").animate({
-//         'scrollTop': $(dest).offset().top - 110
-//     }, 2000);
-// });
-
-
-
-
 $(document).ready(function () {
-	$("a").on('click', function (event) {
+    $("a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
 			var hash = this.hash;
 			$('html, body').animate({
-                scrollTop: $(hash).offset().top -110
+                scrollTop: $(hash).offset().top - 100
 			}, 1000, function () {
-				window.location.hash = hash;
+                window.location.hash = hash;
 			});
 		} 
 	});
 });
 
-
-
-
-
-// (function ($){
-//     $(document).ready(function(){
-//         $('html, body').animate({
-//             'scrollTop': $('href').offset().top
-//         }, 2000);
-//     });
-// })(jQuery);
-
-
-// $(document).ready(() => {
-//     $('nav a').on("click" , function(e){
-//         e.preventDefault();
-//         var top = $($(this).attr('href')).offset().top - 110;
-//         $("html, body").animate({ scrollTop: top }, 800);
-//         console.log(top);
-//     });
-// })
-
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
