ea0911db25f94aec8f9d59a820946576397ed1de
DanVieira7 <gisellademorais@Gisellas-MacBook-Pro.local>
2019-05-06 23:45:24 -0700

    scroll function done

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
 
-
-$(document).ready(function () {
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-			var hash = this.hash;
-			$('html, body').animate({
-                scrollTop: $(hash).offset().top - 100
-			}, 1000, function () {
-                window.location.hash = hash;
-			});
-		} 
-	});
-});
-
-$('.navbar-nav>li>a').on('click', function(){
-    console.log("hitting")
-    $('.navbar-collapse').collapse('hide');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
 
